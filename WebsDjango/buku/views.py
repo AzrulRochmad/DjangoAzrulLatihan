edd4acad1eb6f53c1f6fee5582ecc4bfe5ddf092
Azrul Rochmad <91447980+AzrulRochmad@users.noreply.github.com>
2022-05-21 21:35:22 +0700

    DjangoPython

---
+++ WebsDjango/buku/views.py
@@ -0,0 +1,15 @@
+import imp
+from django.shortcuts import render
+
+# Create your views here.
+
+from.models import Post
+
+def index(request):
+    postingan = Post.objects.all()
+
+    context = {
+        'TampungPostingan':postingan,
+    }
+
+    return render(request, 'buku/index.html', context)
